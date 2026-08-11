2359a9420367f8cc36c5a8237c70d750b56cac4a
anhtuyet1306 <dangtuyet969@gmail.com>
2026-08-11 21:43:07 +0700

    Add about.php with personal introduction and projects

---
+++ about.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <title>Giới thiệu bản thân</title>
+</head>
+<body>
+    <h1>GIỚI THIỆU BẢN THÂN</h1>
+    <h2>Thông tin cá nhân</h2>
+    <p>Họ và tên: Đặng Ánh Tuyết </p>
+    <p>MSSV: 22401843 </p>
+    <p>Lớp: CNTT D2024B </p>
+    <p>Ngành: Công nghệ thông tin</p>
+    <h2>Các dự án đã làm</h2>
+    <ul>
+        <li>Website du lịch Đà Nẵng</li>
+        <li>Hệ thống quản lý chăm sóc thú </li>
+        <li>Thiết kết game giải đố</li>
+    </ul>
+</body>
+</html>
